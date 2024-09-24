eb308a308fa234d210c1c4075f9d556f5d542f39
lucas <lucas@MacBook-Air-de-lucas.local>
2024-09-24 15:20:22 +0200

    push

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
+  imports: [LoginFormComponent]
 })
 export class HomeComponent {
 
