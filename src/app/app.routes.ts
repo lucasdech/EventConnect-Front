2b0aaf1e2f49438f500e7be5f107e16ba34b6983
lucas <lucas@MacBook-Air-de-lucas.local>
2024-09-25 17:22:24 +0200

    push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent},
+  { path: 'about-us', component: AboutUsComponent}
 ];
