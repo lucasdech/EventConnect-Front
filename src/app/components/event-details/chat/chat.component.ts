704f744bcf1da656b7e58f7a876307bc5827cf67
lucas <lucas@MacBook-Air-de-lucas.local>
2024-10-07 15:56:30 +0200

    push

--- src/app/components/event-details/chat/chat.component.ts
+++ src/app/components/event-details/chat/chat.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ChatService } from '../../../services/EventUser/chat.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
+
+  private chatService = inject(ChatService);
+  private route = inject(ActivatedRoute);
+
+  public messages: any[] = [];
+  public eventId: number = 0;
+  public userId: number = 0;
+
+  ngOnInit() {
+    this.userId = +localStorage.getItem('ID')!;
+    this.route.paramMap.subscribe(params => {
+      this.eventId = +params.get('id')!;
+      console.log('ID de l\'événement récupéré :', this.eventId);
+      this.getMessages();
+    });
+  }
+
+  getMessages() {
+    this.chatService.getMessages(this.eventId).subscribe({
+      next: (data) => {
+        this.messages = data;
+        console.log('Messages récupérés :', this.messages);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des messages :', err);
+      }
+    });
+  }
 
 }
---
+++ src/app/services/EventUser/chat.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  private http = inject(HttpClient);
+  private BASE_URL = "https://eventconnectapi.projets.p8.garage404.com/api";
+
+  constructor() { }
+
+  getMessages(eventId: number) {
+    return this.http.get(`${this.BASE_URL}/messages/${eventId}`).pipe(
+      tap((result: any) => {
+        console.log('Réponse Messages:', result);
+      }),
+      map((result: any) => {
+        if (Array.isArray(result?.data['Messages'])) {
+          return result.data['Messages'];
+        } else {
+          console.error('La réponse n\'est pas un tableau valide');
+          return [];
+        }
+      }
+    ));
+  }
+}
