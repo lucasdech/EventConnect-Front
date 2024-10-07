4f0c2405d358b1bd5064830d6b1abf5167cfb49f
lucas <lucas@MacBook-Air-de-lucas.local>
2024-10-07 17:36:07 +0200

    chat ok

--- src/app/components/event-details/chat/chat.component.ts
+++ src/app/components/event-details/chat/chat.component.ts
@@ -1,44 +1,87 @@
 import { Component, inject } from '@angular/core';
-import { ChatService } from '../../../services/EventUser/chat.service';
+import { ChatService, Credentials } from '../../../services/EventUser/chat.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent {
-
   private chatService = inject(ChatService);
   private route = inject(ActivatedRoute);
 
+  messageForm: FormGroup;
+  invalidCredentials = false;
+
   public messages: any[] = [];
   public eventId: number = 0;
-  public userId: number = 0;
+  public userId: number = +localStorage.getItem('ID')!;
+
+  constructor(private fb: FormBuilder) {
+    this.messageForm = this.fb.group({
+      content: ['', Validators.required],
+      event_id: [this.eventId, Validators.required],
+      user_id: [this.userId, Validators.required],
+    });
+  }
 
   ngOnInit() {
-    this.userId = +localStorage.getItem('ID')!;
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.eventId = +params.get('id')!;
-      console.log('ID de l\'événement récupéré :', this.eventId);
+      console.log("ID de l'événement récupéré :", this.eventId);
+      
+      // Mettre à jour le champ event_id dans le formulaire
+      this.messageForm.patchValue({ event_id: this.eventId });
+      
       this.getMessages();
     });
   }
+  
 
   getMessages() {
     this.chatService.getMessages(this.eventId).subscribe({
       next: (data) => {
         this.messages = data;
-        console.log('DATA : ', data)
         console.log('Messages récupérés :', this.messages);
       },
       error: (err) => {
         console.error('Erreur lors de la récupération des messages :', err);
-      }
+      },
     });
   }
 
+  sendMessage() {
+    if (this.messageForm.invalid) return;
+  
+    console.log('Envoi du message :', this.messageForm.value);
+  
+    this.chatService.addMessage(this.messageForm.value as Credentials).subscribe({
+      next: (response: any) => {
+        // Vérifie si la réponse contient le statut 'success'      
+          console.log('Message ajouté:', response);
+          
+          // Vider le champ d'input après l'envoi du message
+          this.messageForm.reset({ 
+            content: '', 
+            event_id: this.eventId, 
+            user_id: this.userId
+          });
+          
+          // Réactualiser la liste des messages après l'envoi
+          this.getMessages();
+
+      },
+      error: (err) => {
+        console.error('Erreur lors de l\'envoi du message :', err);
+        this.invalidCredentials = true;
+      },
+    });
+  }
+  
+  
 }
--- src/app/services/EventUser/chat.service.ts
+++ src/app/services/EventUser/chat.service.ts
@@ -1,36 +1,53 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, tap, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+
+export interface Credentials {
+  content: string;
+  event_id: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class ChatService {
-
   private http = inject(HttpClient);
-  private BASE_URL = "https://eventconnectapi.projets.p8.garage404.com/api";
+  private BASE_URL = 'https://eventconnectapi.projets.p8.garage404.com/api';
 
-  constructor() { }
+  constructor() {}
 
   getMessages(eventId: number) {
-    console.log('Récupération des messages pour l\'événement', eventId);
+    console.log("Récupération des messages pour l'événement", eventId);
     return this.http.get(`${this.BASE_URL}/messages/${eventId}`).pipe(
       tap((result: any) => {
         console.log('Réponse Messages:', result);
       }),
       map((result: any) => {
-        if (Array.isArray(result?.data?.Messages)) {
-          console.log('RESULT DATA : ', result.data.Messages);
-          return result.data.Messages;
+        if (result.data && Array.isArray(result.data.messages)) {
+          return result.data.messages;
         } else {
-          console.error('La réponse n\'est pas un tableau valide');
+          console.error(
+            'Les messages ne sont pas définis ou ne sont pas dans un tableau'
+          );
           return [];
         }
+      })
+    );
+  }
+
+  addMessage(credentials: Credentials): Observable<boolean>  {
+    return this.http.post(`${this.BASE_URL}/message`, credentials).pipe(
+      tap((result: any) => {
+        console.log('Message ajouté:', result);
       }),
-      catchError((error) => {
-        console.error('Erreur lors de la récupération des messages:', error);
-        return of([]);
+      map((result: any) => {
+        if (result.success) {
+          return result.data;
+        } else {
+          console.error("Erreur lors de l'ajout du message");
+          return null;
+        }
       })
     );
   }
