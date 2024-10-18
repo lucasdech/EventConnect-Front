b394d9b2b12f6e930fbfd6f75f979bb35c9447b2
lucas <lucas@MacBook-Air-de-lucas.local>
2024-10-18 17:05:50 +0200

    nav bar change when connected or not ok

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  userId: number = 0;
+  isConnected: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.IsconnectedUser();
+  }
+
+  logout() {
+    localStorage.removeItem('ID');
+    localStorage.removeItem('Participants');
+    localStorage.removeItem('JWT');
+    window.location.href = '/';
+  }
+
+  IsconnectedUser() {
+    if (typeof localStorage.getItem('ID') != undefined && localStorage.getItem('ID') != null) {
+      this.isConnected = true;
+    }else {
+      this.isConnected = false;
+    }
+  }
 
 }
