c7db0a522cf52beeec713886dd2fca9ac89b01e9
lucas <lucas@MacBook-Air-de-lucas.local>
2024-10-24 01:14:09 +0200

    result.succes lol

--- src/app/services/EventUser/chat.service.ts
+++ src/app/services/EventUser/chat.service.ts
@@ -33,7 +33,7 @@ export class ChatService {
         console.log('Message ajouté:', result);
         // await this.supabaseService.insertMessage(messageInput);
       }),
-      map((result: any) => result.success)
+      map((result: any) => result)
     );
   }
 
